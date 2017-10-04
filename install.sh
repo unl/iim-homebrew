7546c12853856c14da0a06b865925047bb449edd
Michael Fairchild <mfairchild365@gmail.com>
2017-10-04 15:25:51 -0500

    account for recent change in httpd
    httpd24 was moved into core as httpd, so we had to adjust this script to account for that change. Also removed some unneeded formula.
    
    We /might/ need the httpd24 formula back, idk yet.

--- install.sh
+++ install.sh
@@ -17,22 +17,17 @@ for php in php53 php54 php55 php56; do
 done
 
 # Install common taps
-brew tap homebrew/apache
 brew tap homebrew/dupes
 brew tap homebrew/php
 brew tap homebrew/services
 brew tap homebrew/versions
-
-echo "Copying patched formulae from kabel"
-cp formula/httpd24.rb /usr/local/Library/Taps/homebrew/homebrew-apache/
-cp formula/php70* /usr/local/Library/Taps/homebrew/homebrew-php/Formula
 
 # Install common formulae
 brew install Caskroom/cask/xquartz
 brew install --with-openssl curl
 brew install gd
 brew install git
-brew install httpd24 --with-privileged-ports --with-mpm-event --with-http2 --with-ldap
+brew install httpd
 brew install imagemagick --with-librsvg --with-webp --with-libwmf --with-liblqr --with-fontconfig --with-ghostscript --with-jp2 --with-x11
 brew install jpegoptim
 brew install links
@@ -43,14 +38,13 @@ brew install --with-homebrew-curl php70
 brew install php70-intl php70-mcrypt php70-opcache php70-tidy php70-xdebug
 brew install --HEAD php70-memcached
 brew install --HEAD php70-imagick
-brew install --HEAD php70-xhprof
 brew install phpmd
 brew install composer
 brew install php-code-sniffer
 brew install pngquant
 brew install wget
 
-if [ ! -d /usr/local/etc/apache2/2.4 ]; then
+if [ ! -d /usr/local/etc/httpd ]; then
 	echo "Failed to properly install apache"
 	exit 1
 fi
@@ -62,7 +56,7 @@ fi
 
 # Copy patched configuration
 echo "Copying common Apache and PHP configuration. It is okay to skip these if you do not want to override your local copy."
-cp -ai etc/httpd24/* /usr/local/etc/apache2/2.4
+cp -ai etc/httpd/* /usr/local/etc/httpd
 cp -ai etc/php70/* /usr/local/etc/php/7.0
 
 # Start system daemons
@@ -73,7 +67,7 @@ sudo brew services restart memcached
 echo "Starting Homebrew PHP-FPM on a unix socket..."
 sudo brew services restart php70
 echo "starting Homebrew Apache on default ports..."
-sudo brew services restart httpd24
+sudo brew services restart httpd
 
 echo "✩✩✩✩ IIM OS X Web Stack - Ready ✩✩✩✩"
 echo ""
